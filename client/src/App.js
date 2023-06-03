5c20412f8baa176087bf8862a2ac9f0e9dae1776
Ben Whitfield-Heap <benjaminpheap@gmail.com>
2023-06-03 08:59:54 +0100

    Styling and adding of progress bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 import Button from '@mui/material/Button'; // Import Button from Material UI
 import TextField from '@mui/material/TextField';
@@ -7,51 +7,66 @@ import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
-import { AppBar, Grid, Container, Toolbar, Typography, Card, CardContent } from '@mui/material';
+import { AppBar, Container, LinearProgress, Toolbar, Typography, Card, CardContent } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+
 import './App.css';
+import background from './images/background.png';
 
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#a5d6a7', // Soft Green
+      main: '#f6f1c1',
     },
     secondary: {
-      main: '#fff59d', // Light Yellow
+      main: '#c1d6f6', 
     },
+    success: {
+      main: '#a5d6a7'
+    }
   },
 });
 
-
 function App() {
+  const [loading, setLoading] = useState(false);
   const [story, setStory] = useState('');
   const [character, setCharacter] = useState('A dragon named Albert');
   const [minutes, setMinutes] = useState(2);
   const [age, setAge] = useState(2);
   const [moral, setMoral] = useState('to be good');
+  const [showForm, setShowForm] = useState(true);
 
   const createStory = async () => {
+    setShowForm(false);
+    setLoading(true);
     const prompt = `
-        Act as a best selling children's author. Write a bedtime story designed for a child of age ${age}. 
-        It should take the parent around ${minutes} minutes to read the story. 
-        The main character is ${character}. The hidden moral of the story will be ${moral}. 
+        Create a bedtime story designed for a child of age ${age}. 
+        The story should take about ${minutes} minutes to read to the child. 
+        The main character is ${character}. The hidden moral will be ${moral}. 
         Always conclude the Story with "The End".
-        Return the content formatted in HTML, rather than plain text.
+        Return the content formatted in HTML. Use paragraph tags to make it easy to read.
         Each Story should have a short title at the start in H2. \n\n 
       `;
     try {
       const res = await axios.post('http://localhost:5000/create-story', { prompt });
       setStory(res.data.story);
+      setLoading(false);
     } catch (error) {
       console.error(error);
       setStory('Error creating story');
+      setLoading(false);
     }
+  }
+
+  const startNewStory = () => {
+    window.location.reload();
   }
 
   return (
     <ThemeProvider theme={theme}>
-      <header>
+      {/* <header>
         <AppBar position='static'>
           <Toolbar>
             <Typography variant="h6">
@@ -59,101 +74,116 @@ function App() {
             </Typography>
           </Toolbar>
         </AppBar>
-      </header>
+      </header> */}
       <main>
         <section class='intro'>
           <Container maxWidth='sm' align='center'>
             <Typography variant="h1">Bonnie</Typography>
             <Typography variant="p">
-              Welcome to Bonnie, your digital bedtime story companion. 
+              Create a bedtime story for your loved one
+              {/* Welcome to Bonnie, your digital bedtime story companion. 
               Bonnie is designed to make bedtime stories more engaging, creative, and personalized for your little ones. 
               With Bonnie, you can quickly craft unique tales tailored to your child's interests and imagination. 
               Whether it's an adventure in a magical kingdom or a journey through the stars, 
               Bonnie helps you transform bedtime into an unforgettable storytelling experience. 
-              Unlock the world of imagination with Bonnie, and make each night a special memory for your child.
+              Unlock the world of imagination with Bonnie, and make each night a special memory for your child. */}
             </Typography>
           </Container>
         </section>
-        <section className='story-form'>
-          <Container maxWidth='sm'>
-            <label htmlFor="character">Describe the main character:</label>
+        {showForm && (
+          <section class='story-form'>
+            <Container maxWidth='sm'>
+              <Stack spacing={5}>
+                  <label htmlFor="character">Describe the main character:</label>
+                  <TextField
+                    value={character}
+                    onChange={e => setCharacter(e.target.value)}
+                    label="Character" // Display label for the TextField
+                    variant="outlined" // Outlined style
+                    fullWidth="True"
+                    color="success"
+                  />
+                  <label htmlFor="minutes">Length of the story (in minutes):</label>
+                  <Slider
+                    aria-label="Minutes"
+                    defaultValue={2}
+                    value={minutes}
+                    valueLabelDisplay="on"
+                    step={1}
+                    marks
+                    min={1}
+                    max={5}
+                    color="success"
+                    onChange={(e, value) => setMinutes(value)}
+                  />
+                <label htmlFor="age">Age of the child:</label>
+                <FormControl variant="outlined">
+                  <InputLabel id="age-label">Years</InputLabel>
+                  <Select
+                    labelId="age-label"
+                    id="age"
+                    value={age}
+                    onChange={e => setAge(e.target.value)}
+                    label="Age of the child"
+                    color="success"
 
-            {/* <Grid container direction="column" spacing={2}> */}
-              {/* <Grid item> */}
-              <TextField
-                value={character}
-                onChange={e => setCharacter(e.target.value)}
-                label="Character" // Display label for the TextField
-                variant="outlined" // Outlined style
-              />
-              {/* </Grid> */}
-              {/* <Grid item> */}
-              <label htmlFor="minutes">Length of the story (in minutes):</label>
-              <Slider
-                aria-label="Minutes"
-                defaultValue={2}
-                value={minutes}
-                valueLabelDisplay="auto"
-                step={1}
-                marks
-                min={1}
-                max={5}
-                onChange={(e, value) => setMinutes(value)}
-              />
-              {/* </Grid>
-              <Grid item> */}
-              <label htmlFor="age">Age of the child:</label>
-              <FormControl variant="outlined">
-                <InputLabel id="age-label">Years</InputLabel>
-                <Select
-                  labelId="age-label"
-                  id="age"
-                  value={age}
-                  onChange={e => setAge(e.target.value)}
-                  label="Age of the child"
-                >
-                  <MenuItem value={"Under 1"}>Under 1</MenuItem>
-                  <MenuItem value={1}>1</MenuItem>
-                  <MenuItem value={2}>2</MenuItem>
-                  <MenuItem value={3}>3</MenuItem>
-                  <MenuItem value={4}>4</MenuItem>
-                  <MenuItem value={5}>5</MenuItem>
-                  <MenuItem value={6}>6</MenuItem>
-                  <MenuItem value={7}>7</MenuItem>
-                  <MenuItem value={8}>8</MenuItem>
-                  <MenuItem value={9}>9+</MenuItem>
-                </Select>
-              </FormControl>
-              {/* </Grid>
-              <Grid item> */}
-              <label htmlFor="moral">Moral of the story:</label>
-              <TextField
-                value={moral}
-                onChange={e => setMoral(e.target.value)}
-                label="Moral of Story" // Display label for the TextField
-                variant="outlined" // Outlined style
-              />
-              {/* </Grid>
-              <Grid item> */}
-                <Button variant="contained" onClick={createStory}>Create a Story</Button>
-              {/* </Grid> */}
-
-              {/* <Grid item>
-              </Grid>
-            </Grid> */}
-          </Container>
-        </section>
-        <section className='story-output'>
-          <Container maxWidth='sm'>
-            <Card>
-              <CardContent>
-                <Typography variant="body1" paragraph={true} align="left" color="textPrimary">
-                  <div dangerouslySetInnerHTML={{ __html: story }} />
-                </Typography>        
-              </CardContent>
-            </Card>
-          </Container>
-        </section>
+                  >
+                    <MenuItem value={"Under 1"}>Under 1</MenuItem>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                    <MenuItem value={6}>6</MenuItem>
+                    <MenuItem value={7}>7</MenuItem>
+                    <MenuItem value={8}>8</MenuItem>
+                    <MenuItem value={9}>9+</MenuItem>
+                  </Select>
+                </FormControl>
+                <label htmlFor="moral">Moral of the story:</label>
+                <TextField
+                  value={moral}
+                  onChange={e => setMoral(e.target.value)}
+                  label="Moral of Story" // Display label for the TextField
+                  variant="outlined" // Outlined style
+                  color="success"
+                />
+                  <Button variant="contained" onClick={createStory} color="success">Create a Story</Button>
+                </Stack>
+            </Container>
+          </section>
+        )}
+        {loading ? (
+          <section class='story-output'>
+            <Container maxWidth='sm'>
+              <Card>
+                <CardContent>
+                  <Stack spacing={2}>
+                    <LinearProgress />
+                    <Typography align="center">Creating your new story...</Typography>
+                  </Stack>
+                </CardContent>
+              </Card>
+            </Container>
+          </section>
+        ) : (
+          story && (
+            <section class='story-output'>
+              <Container maxWidth='sm'>
+                <Stack spacing={5}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="body1" paragraph={true} align="left" color="textPrimary">
+                      <div dangerouslySetInnerHTML={{ __html: story }} />
+                    </Typography> 
+                  </CardContent>
+                </Card>
+                <Button variant="contained" onClick={startNewStory} color="success">Start a New Story</Button>
+                </Stack>
+              </Container>
+            </section>
+          )
+        )}
       </main>
     </ThemeProvider>
   );
--- server.js
+++ server.js
@@ -12,20 +12,45 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+// TEXT-DAVINCI-003
+
+// app.post('/create-story', async (req, res) => {
+//   try {
+//     const prompt = req.body.prompt;
+//     const completion = await openai.createCompletion({
+//       model: "text-davinci-003",
+//       prompt: prompt,
+//       max_tokens: 3000, // Adjust as needed
+//     });
+//     res.json({ story: completion.data.choices[0].text });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Error creating story' });
+//   }
+// });
+
+
+// gpt-3.5-turbo
+
 app.post('/create-story', async (req, res) => {
   try {
     const prompt = req.body.prompt;
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      max_tokens: 3000, // Adjust as needed
+    const messages = [
+      { role: 'system', content: 'You are a skilled writer of stories for children.' },
+      { role: 'user', content: prompt },
+    ];
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages,
     });
-    res.json({ story: completion.data.choices[0].text });
+    res.json({ story: completion.data.choices[0].message.content });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error creating story' });
   }
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
