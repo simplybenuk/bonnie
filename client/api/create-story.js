449f0865154f9b7aafd258ce42303c08a3b571e0
Ben Whitfield-Heap <benjaminpheap@gmail.com>
2023-06-03 22:44:11 +0100

    Update API for vercel

--- client/api/create-story.js
+++ client/api/create-story.js
@@ -1,10 +1,9 @@
 // /api/create-story.js
-require('dotenv').config();
 const cors = require('cors');
 const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.API_URL,
 });
 const openai = new OpenAIApi(configuration);
 
